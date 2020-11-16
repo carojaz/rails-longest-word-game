2d62750f14255be1578a64d1368a5831757f41e8
caroline jasinski <jasinski_caroline@yahoo.fr>
2020-11-16 16:59:04 +0100

    longest word

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,41 @@
+require 'open-uri'
+
+class GamesController < ApplicationController
+  def new
+    @grid = []
+    n = 0
+    while n < 10
+      @grid << ('A'..'Z').to_a.sample
+      n += 1
+    end
+  end
+
+  def count(letters)
+    letter_frequencies = Hash.new(0)
+    if letters.is_a? Array
+      letters.each { |letter| letter_frequencies[letter] += 1 }
+    else
+      letters_separated = letters.split("")
+      letters_separated.each { |letter| letter_frequencies[letter] += 1 }
+    end
+    return letter_frequencies
+  end
+
+  def score
+    @attempt = params[:word]
+    url = "https://wagon-dictionary.herokuapp.com/#{@attempt}"
+    open_url = open(url)
+    @word = JSON.parse(open(url).read)
+    ok = 0
+    count(@attempt.upcase).each { |letter, frequency| frequency <= count(params[:letters])[letter] ? ok += 0 : ok += 1 }
+    if @word["found"] && ok.zero?
+      @result = { score: @word["length"], message: "Well done!"}
+    elsif @word["found"] == false && ok.zero?
+      @result = { score: 0, message: "Nice try but #{@attempt} is not an english word!" }
+    else
+      @result = { score: 0, message: "Nice try but in #{@attempt} some letters are not in the grid!" }
+    end
+    session[:score] ? session[:score] << @result[:score] : session[:score] = [@result[:score]]
+    @total = session[:score].reduce(0, :+)
+  end
+end
---
+++ test/system/games_test.rb
@@ -0,0 +1,18 @@
+require "application_system_test_case"
+
+class GamesTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit games_url
+  #
+  #   assert_selector "h1", text: "Game"
+  # end
+  test "Going to /new gives us a new random grid to play with" do
+    visit new_url
+    assert test: "New game"
+    assert_selector "li", count: 10
+  end
+
+  test "Filling the form with a random word, click the play button, and get a message that the word is not in the grid" do
+    click_on "Play"
+  end
+end
